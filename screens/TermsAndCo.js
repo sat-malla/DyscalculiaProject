c7a8f89e84d83bc1d496be4d8299ca96e069f679
sat-malla <57183091+sat-malla@users.noreply.github.com>
2023-09-03 22:30:18 -0700

    Did some TAC

--- screens/TermsAndCo.js
+++ screens/TermsAndCo.js
@@ -27,9 +27,73 @@ const TermsAndCo = () => {
       >
         Terms & Conditions
       </Text>
-      <Text>
-        
+      <Text
+        style={{
+          fontSize: 20,
+          marginTop: 50,
+          color: colors.text,
+          textAlign: "center"
+        }}
+      >
+        Welcome to LearnCulia!
       </Text>
+      <Text
+        style={{
+          fontSize: 20,
+          marginTop: 50,
+          color: colors.text,
+          textAlign: "center"
+        }}
+      >
+        These terms and conditions outline the rules and regulations for the use
+        of LearnCulia.
+      </Text>
+      <Text
+        style={{
+          fontSize: 20,
+          marginTop: 50,
+          color: colors.text,
+          textAlign: "center"
+        }}
+      >
+        By using this app we assume you accept these terms and conditions. Do
+        not continue to use LearnCulia if you do not agree to take all of the
+        terms and conditions stated on this page.
+      </Text>
+      <Text
+        style={{
+          fontSize: 20,
+          marginTop: 50,
+          color: colors.text,
+          textAlign: "center"
+        }}
+      >
+        The following terminology applies to these Terms and Conditions, Privacy
+        Statement and Disclaimer Notice and all Agreements: "Client", "You" and
+        "Your" refers to you, the person log on this website and compliant to
+        the Company’s terms and conditions. "The Company", "Ourselves", "We",
+        "Our" and "Us", refers to our Company. "Party", "Parties", or "Us",
+        refers to both the Client and ourselves. All terms refer to the offer,
+        acceptance and consideration of payment necessary to undertake the
+        process of our assistance to the Client in the most appropriate manner
+        for the express purpose of meeting the Client’s needs in respect of
+        provision of the Company’s stated services, in accordance with and
+        subject to, prevailing law of Netherlands. Any use of the above
+        terminology or other words in the singular, plural, capitalization
+        and/or he/she or they, are taken as interchangeable and therefore as
+        referring to the same.
+      </Text>
+      <Text
+        style={{
+          fontSize: 20,
+          fontWeight: "bold",
+          marginTop: 50,
+          color: colors.text,
+        }}
+      >
+        License
+      </Text>
+      <View style={{ height: 70 }} />
     </ScrollView>
   );
 };
